7276377fe2016e67f536f664bb87e8dc125a06b7
caojianhua <caojianhua1741@gmail.com>
2014-11-07 11:19:25 +0800

    Remove pods from git track

--- Example/Pods/Target Support Files/Pods-JHAvatarAggregation-JHAvatarAggregation/Pods-JHAvatarAggregation-JHAvatarAggregation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JHAvatarAggregation_JHAvatarAggregation : NSObject
-@end
-@implementation PodsDummy_Pods_JHAvatarAggregation_JHAvatarAggregation
-@end
